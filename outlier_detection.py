70c40460aaaf094916913a00bf9474dd2732925d
Aimee Barciauskas <aimee@developmentseed.org>
2018-06-03 19:53:50 -0400

    Some outlier detection cleanup

--- outlier_detection.py
+++ outlier_detection.py
@@ -16,27 +16,16 @@ from uszipcode import ZipcodeSearchEngine
 
 class OutlierDetection(object):
     def __init__(self, us_data_pm25_df):
-        self.us_data_pm25_df = us_data_pm25_df
-
-    
-    def extract_time( self ):
-        date_map_df = pd.DataFrame(self.us_data_pm25_df["local"].drop_duplicates())
-
-        # creating a datetime format
-        date_map_df["local_datetime"] = pd.to_datetime(date_map_df["local"])
-
-        # extracting the time and day
-        date_map_df["local_hour"] = date_map_df["local_datetime"].dt.hour
-        date_map_df["local_day"] = date_map_df["local_datetime"].dt.day
-
-        self.date_map_df = date_map_df
-    
+        self.us_data_pm25_df = pd.DataFrame(us_data_pm25_df)
+        self.us_data_pm25_df['local_datetime'] = pd.to_datetime(self.us_data_pm25_df['local'])
+        self.us_data_pm25_df['local_hour'] = self.us_data_pm25_df['local_datetime'].dt.hour
+        self.us_data_pm25_df['local_day'] = self.us_data_pm25_df['local_datetime'].dt.day   
 
     def coords_to_df( self ):
         # Create a DataFrame of Unique Location Coordinates of ALL sensors in the US
         coords_df = self.us_data_pm25_df[['location','latitude', 'longitude']].drop_duplicates()\
                                                                     .reset_index()\
-                                                                    .drop("index",axis=1)
+                                                                    .drop('index',axis=1)
         # Prep the Coordinate DF for a distance matrix
         coords = coords_df.as_matrix(columns=['latitude', 'longitude'])
         self.coords_df, self.coords = coords_df, coords
@@ -70,9 +59,9 @@ class OutlierDetection(object):
             cluster_sil_means.append( silhouette_avg )
 
         clusters_df = pd.DataFrame( {\
-                                       "num_clusters":cluster_range\
-                                     , "cluster_errors": cluster_errors
-                                     , "mean_sil": cluster_sil_means } )
+                                       'num_clusters':cluster_range\
+                                     , 'cluster_errors': cluster_errors
+                                     , 'mean_sil': cluster_sil_means } )
 
         # return clusters_df,cluster_errors,cluster_sil_means,X
         self.clusters_df,self.cluster_errors,self.cluster_sil_means,self.X =\
@@ -97,29 +86,22 @@ class OutlierDetection(object):
         self.labels, self.sil_info, self.k = labels, sil_info, k
 
     def merge_labels( self ):
-        
-        coords_df = self.coords_df
-        us_data_pm25_df = self.us_data_pm25_df
-        labels = self.labels
-        
-        coords_df["geo_cluster"] = labels
-
+        self.coords_df['geo_cluster'] = self.labels
         # Merge Geo Clusters in on lat lng for precision
-        working_df = us_data_pm25_df.merge(\
-                                             coords_df[["latitude","longitude","location","geo_cluster"]]\
-                                           , on=["latitude","longitude","location"], how="left" )
-
-        self.working_df, self.coords_df = working_df, coords_df
+        working_df = self.us_data_pm25_df.merge(self.coords_df[['latitude','longitude','location','geo_cluster']], \
+                                                on=['latitude','longitude','location'], how='left' )
+        self.working_df = working_df
 
     def dist_matrix( self ):
+        print('indistancematrix')
         working_df = self.working_df
 
         # Calculate the mid point
         midpoints_df = working_df[[\
-                                     "geo_cluster"\
-                                   , "latitude"\
-                                   , "longitude"\
-                                  ]].groupby("geo_cluster")\
+                                     'geo_cluster'\
+                                   , 'latitude'\
+                                   , 'longitude'\
+                                  ]].groupby('geo_cluster')\
                                     .mean()\
                                     .reset_index()
 
@@ -143,24 +125,24 @@ class OutlierDetection(object):
     def neighbor_lookup( self, neighbor_distance ):
         dist_matrix_dummies = self.dist_matrix_km.applymap(lambda x: self.flag_neighbor(x, neighbor_distance))\
                                             .reset_index()\
-                                            .rename(columns={"index":"geo_cluster"})
+                                            .rename(columns={'index':'geo_cluster'})
         
         # Create a dictionary that contains a list of neighbors for each cluster
         # We can pass this in as a filter to make it easier to map model fit logic later
         neighbor_dict = {}
 
         # Loop through the unique clusters
-        for cluster in dist_matrix_dummies["geo_cluster"].unique():
+        for cluster in dist_matrix_dummies['geo_cluster'].unique():
             # Transpose the row into a column vector
-            neighbor_flags = dist_matrix_dummies[dist_matrix_dummies["geo_cluster"]==cluster].iloc[:, 1:].T
+            neighbor_flags = dist_matrix_dummies[dist_matrix_dummies['geo_cluster']==cluster].iloc[:, 1:].T
             neighbor_flags = neighbor_flags.rename(\
                                                    columns={\
-                                                              neighbor_flags.columns.values[0]:"neighbor_ind"\
+                                                              neighbor_flags.columns.values[0]:'neighbor_ind'\
                                                             })\
 
 
             # When there is a neighbor flag, take the index and store it in a dictionary
-            neighbor_list = list(neighbor_flags[neighbor_flags["neighbor_ind"]==1].index)
+            neighbor_list = list(neighbor_flags[neighbor_flags['neighbor_ind']==1].index)
             neighbor_dict[cluster] = neighbor_list
 
         self.neighbor_dict = neighbor_dict
@@ -169,7 +151,7 @@ class OutlierDetection(object):
         midpoints_df = self.midpoints_df
         
         search = ZipcodeSearchEngine()
-        midpoints_df["City"] = midpoints_df[["latitude", "longitude"]].apply(lambda x:\
+        midpoints_df['City'] = midpoints_df[['latitude', 'longitude']].apply(lambda x:\
                                                       search.by_coordinate(\
                                                                               x[0]\
                                                                            , x[1]\
@@ -177,7 +159,7 @@ class OutlierDetection(object):
                                                                            , returns=1)[0].City\
                                                       , axis=1)
 
-        midpoints_df["State"] = midpoints_df[["latitude", "longitude"]].apply(lambda x:\
+        midpoints_df['State'] = midpoints_df[['latitude', 'longitude']].apply(lambda x:\
                                                       search.by_coordinate(\
                                                                               x[0]\
                                                                            , x[1]\
@@ -185,20 +167,20 @@ class OutlierDetection(object):
                                                                            , returns=1)[0].State\
                                                       , axis=1)
 
-        midpoints_df["City_State"] =midpoints_df["City"]+", "+midpoints_df["State"]
-        cities_dict = midpoints_df.set_index("geo_cluster").to_dict("index")
+        midpoints_df['City_State'] =midpoints_df['City']+', '+midpoints_df['State']
+        cities_dict = midpoints_df.set_index('geo_cluster').to_dict('index')
         self.cities_dict = cities_dict
 
     def resample_and_unstack( self ):
         working_df = self.working_df
         
-        grouped_ts_df = working_df[working_df["local_hour"]==18]\
-                                    [["geo_cluster","location","local_datetime","value"]]\
-                                        .groupby(["geo_cluster", "local_datetime"])\
+        grouped_ts_df = working_df[working_df['local_hour']==18]\
+                                    [['geo_cluster','location','local_datetime','value']]\
+                                        .groupby(['geo_cluster', 'local_datetime'])\
                                         .mean()\
                                         .reset_index()
 
-        df = grouped_ts_df[["geo_cluster","local_datetime","value"]].drop_duplicates()\
+        df = grouped_ts_df[['geo_cluster','local_datetime','value']].drop_duplicates()\
                                                                     .copy()
 
         # set a datetime64 dtype to the date, so that we can use it as an index    
@@ -209,7 +191,7 @@ class OutlierDetection(object):
 
         # create a generator function for groupby operation, that resamples to a 1 day
         ## for the entire dataset, and then unstack it into a column for each cluster
-        grouper = df.groupby([pd.Grouper(freq='1D'), "geo_cluster"])
+        grouper = df.groupby([pd.Grouper(freq='1D'), 'geo_cluster'])
         unstacked_df = grouper['value'].sum().unstack('geo_cluster')
         self.unstacked_df = unstacked_df
 
@@ -224,17 +206,17 @@ class OutlierDetection(object):
         f = lambda x: 1 if x==4 else 0
         window_null_ct_df = window_null_ct_df.applymap( f ).reset_index()
 
-        window_null_ct_df["year"] = window_null_ct_df["local_datetime"].map( lambda x: x.year )
+        window_null_ct_df['year'] = window_null_ct_df['local_datetime'].map( lambda x: x.year )
 
-        year_ct = window_null_ct_df.groupby("year")\
+        year_ct = window_null_ct_df.groupby('year')\
                          .count()
 
-        year_sum = window_null_ct_df.groupby("year")\
+        year_sum = window_null_ct_df.groupby('year')\
                           .sum()
 
         window_null_rates = (year_sum/year_ct).T[:-1].reset_index()
 
-        drop_list = list(window_null_rates[window_null_rates[2016]>.2]["geo_cluster"])
+        drop_list = list(window_null_rates[window_null_rates[2016]>.2]['geo_cluster'])
         keep_list = [i for i in unstacked_df.columns if i not in drop_list]
         self.keep_list, self.drop_list, self.window_null_rates = keep_list, drop_list, window_null_rates
 
@@ -258,12 +240,12 @@ class OutlierDetection(object):
         w = lowess(y, x, frac=1./3)
 
         f = lambda x: x[0]
-        output_df = pd.DataFrame(map(f,w[:,1:]), columns=["LOWESS"])
-        output_df["local_datetime"]=x
+        output_df = pd.DataFrame(map(f,w[:,1:]), columns=['LOWESS'])
+        output_df['local_datetime']=x
 
-        merged_df=dates_df.merge(output_df, on="local_datetime", how="left")
+        merged_df=dates_df.merge(output_df, on='local_datetime', how='left')
 
-        return merged_df["LOWESS"]
+        return merged_df['LOWESS']
 
     def calc_sq_residuals( self, unstacked_df ):
 
@@ -282,7 +264,7 @@ class OutlierDetection(object):
                       eps=.9\
                     , min_samples=4\
                     , leaf_size=30\
-                    , metric="euclidean"\
+                    , metric='euclidean'\
                    ).fit(X)
 
         core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
